705acbf4448056ed21ffed21650c5fc91285fae5
Anna Baginski <annabaginski.dev@gmail.com>
2023-01-02 15:50:18 -0500

    additional parameters added to user schemaaaaaaaaa

--- models/User.js
+++ models/User.js
@@ -6,6 +6,8 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
   healthcareWorker: Boolean,
+  intakeCompleted: Boolean,
+  isolationStatus: Boolean,
 });
 
 // Password hash middleware.
